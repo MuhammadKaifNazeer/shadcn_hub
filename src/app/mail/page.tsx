af65a4b9555633d495e677461a38f982f731c9bc
Muhammad Kaif Nazeer <122388541+MuhammadKaifNazeer@users.noreply.github.com>
2024-07-18 03:17:30 +0000

    mail

---
+++ src/app/mail/page.tsx
@@ -0,0 +1,27 @@
+import { cookies } from "next/headers";
+import Image from "next/image";
+
+import { Mail } from "@/components/mail/mail";
+import { accounts, mails } from "@/data/data";
+
+export default function Home() {
+  const layout = cookies().get("react-resizable-panels:layout");
+  const collapsed = cookies().get("react-resizable-panels:collapsed");
+
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+  const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
+
+  return (
+    <>
+      <div className="flex-col flex">
+        <Mail
+          accounts={accounts}
+          mails={mails}
+          defaultLayout={defaultLayout}
+          defaultCollapsed={defaultCollapsed}
+          navCollapsedSize={4}
+        />
+      </div>
+    </>
+  );
+}
